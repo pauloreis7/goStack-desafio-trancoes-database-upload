c47650aa26cf6024a119542e03de7e2df5b537e0
Paulo Silva dos Reis <Paulosilvadosreis2057@gmail.com>
2021-01-19 22:56:02 -0300

    Creaiação e listagem de transações adicionado

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,47 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm'
 
+import TransactionsRepository from '../repositories/TransactionsRepository'
+
+// import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from  '../models/Category'
+
+interface Request {
+  title: string, 
+  value: number, 
+  type: 'income' | 'outcome', 
+  category: string
+} 
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({ title, value, type, category }: Request): Promise<Transaction> {
+    
+    const transactionsRepository = getCustomRepository(TransactionsRepository)
+    const categoryRepository = getRepository(Category)
+
+    let transactionCategory = await categoryRepository.findOne({ 
+      where: { title: category }
+    })
+
+    if(!transactionCategory) {
+
+      transactionCategory = categoryRepository.create({
+        title: category
+      })
+
+      await categoryRepository.save(transactionCategory)
+    }
+
+    const transaction = transactionsRepository.create({
+      title, 
+      value, 
+      type,
+      category: transactionCategory
+    })
+
+    await transactionsRepository.save(transaction)
+
+    return transaction
   }
 }
 
